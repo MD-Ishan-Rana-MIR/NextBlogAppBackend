3069fe29572641210f199cdd93b271d1a4125aa3
ishanrana363 <ishanrana363@gmail.com>
2025-08-17 18:48:01 +0600

    user login api modify

--- src/module/auth/authApi.ts
+++ src/module/auth/authApi.ts
@@ -1,13 +1,14 @@
 import express from 'express';
-import { userLogin, userLogout, userProfile, userRegistration } from './authController';
+import { userLogin, userLogout, userProfile, userProfileUpdate, userRegistration } from './authController';
 import { isLogin } from '../../middleware/authMiddleware';
 
 const router = express.Router();
 
 router.post('/register', userRegistration);
 router.post('/login', userLogin);
-router.get(`/user-profile`, isLogin,userProfile );
-router.get(`/logout`, isLogin, userLogout  );
+router.get(`/user-profile`, isLogin, userProfile);
+router.get(`/logout`, isLogin, userLogout);
+router.put(`/profile-update`, isLogin, userProfileUpdate);
 
 
 
